654b8446e00fa161cacd74493efb375a231169d4
mikaeladev <100416544+mikaeladev@users.noreply.github.com>
2026-02-08 16:14:10 +0000

    final prototype

--- src/main.rs
+++ src/main.rs
@@ -1,128 +1,38 @@
-use std::{
-  collections::HashMap,
-  error::Error,
-  fs::{File, OpenOptions},
-  io::{Stdout, Write, stdout},
-  path::PathBuf,
-};
-
-use svg::{
-  Document, Node,
-  node::element::{Group, Path, Rectangle, path::Data},
-};
+mod outputs;
+mod pixels;
 
 use clap::{Parser, ValueEnum};
+use outputs::Output;
+use pixels::{PixelData, ShapeData};
+use std::{collections::HashMap, error::Error, path::PathBuf};
+use svg::{Document, Node, node::element};
+
+#[derive(Clone, Copy, ValueEnum)]
+enum Method {
+  /// Uses `<path>` elements to draw connected shapes out of line segments;
+  /// results in a small file size and efficient rendering (recommended)
+  Polygons,
+  /// Uses `<rect>` elements to draw rows of rectangles; similar to pixels but
+  /// with adjacent pixels of the same colour merged into a larger block
+  Blocks,
+  /// Uses `<rect>` elements to plot individual pixels one by one; results in a
+  /// much larger file size, but an easily editable result
+  Pixels,
+}
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
   /// Path to the input file
   input: PathBuf,
+
   /// Path to the output file or '-' for stdout
   #[arg(short = 'O', long = "output")]
   output: Option<PathBuf>,
-  /// Method used to represent pixels
+
+  /// Method used to generate the image
   #[arg(short = 'm', long = "method", default_value = "polygons")]
   method: Method,
-}
-
-#[derive(Clone, Copy, ValueEnum)]
-enum Method {
-  /// Uses `<path>` elements to draw connected shapes out of line segments;
-  /// results in a smaller file size and more efficient rendering
-  Polygons,
-  /// Uses `<rect>` elements to represent individual pixels; results in a much
-  /// larger file size, but can be useful if you want to import the SVG in a
-  /// pixel art editor
-  Rectangles,
-}
-
-enum Output {
-  File(File),
-  Stdout(Stdout),
-}
-
-impl Write for Output {
-  fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-    match self {
-      Output::File(f) => f.write(buf),
-      Output::Stdout(s) => s.write(buf),
-    }
-  }
-
-  fn flush(&mut self) -> std::io::Result<()> {
-    match self {
-      Output::File(f) => f.flush(),
-      Output::Stdout(s) => s.flush(),
-    }
-  }
-}
-
-impl Output {
-  fn new(path: PathBuf) -> std::io::Result<Self> {
-    if path.to_str() == Some("-") {
-      Ok(Output::Stdout(stdout()))
-    } else {
-      Ok(Output::File(
-        OpenOptions::new()
-          .create(true)
-          .truncate(true)
-          .write(true)
-          .open(path)?,
-      ))
-    }
-  }
-}
-
-fn to_hexcode(colours: &[u8]) -> String {
-  let rgb = format!("#{:02x}{:02x}{:02x}", colours[0], colours[1], colours[2]);
-
-  if colours[3] < 0xFF {
-    return format!("{rgb}{:02x}", colours[3]);
-  } else {
-    return rgb;
-  }
-}
-
-fn to_px(value: u32) -> String {
-  format!("{value}px")
-}
-
-fn to_path_element(hexcode: &str, coords: &Vec<(u32, u32)>) -> Path {
-  let mut data = Data::new();
-
-  for (x, y) in coords {
-    let (x, y) = (*x, *y);
-
-    data = data
-      .move_to((x, y))
-      .line_to((x + 1, y))
-      .line_to((x + 1, y + 1))
-      .line_to((x, y + 1));
-  }
-
-  data = data.close();
-
-  Path::new().set("fill", hexcode).set("d", data)
-}
-
-fn to_rect_element(hexcode: &str, coords: &Vec<(u32, u32)>) -> Group {
-  let mut group = Group::new();
-
-  for (x, y) in coords {
-    let (x, y) = (*x, *y);
-
-    let rect = Rectangle::new()
-      .set("x", to_px(x))
-      .set("y", to_px(y))
-      .set("width", to_px(1))
-      .set("height", to_px(1))
-      .set("fill", hexcode);
-
-    group.append(rect);
-  }
-
-  group
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -136,26 +46,69 @@ fn main() -> Result<(), Box<dyn Error>> {
 
   let image_file = image::open(input)?;
   let image_data = image_file.to_rgba8();
+  let image_pixels = image_data.enumerate_pixels().filter(|(_, _, c)| c[3] > 0);
 
-  let pixel_data = image_data.enumerate_pixels().filter(|(_, _, c)| c[3] > 0);
+  let mut shape_data: HashMap<String, Vec<ShapeData>> = HashMap::new();
 
-  let mut pixel_map: HashMap<String, Vec<(u32, u32)>> = HashMap::new();
+  for (x, y, c) in image_pixels {
+    let pixel = PixelData::new(x, y, c);
+    let shape = ShapeData::new(&pixel);
 
-  for (x, y, c) in pixel_data {
-    let hexcode = to_hexcode(&c.0);
-    let coords = pixel_map.entry(hexcode).or_default();
+    shape_data
+      .entry(pixel.hexcode)
+      .and_modify(|vec| {
+        let new_shape = match method {
+          Method::Pixels => shape,
+          Method::Polygons | Method::Blocks => {
+            let last_shape = vec.pop_if(|s| s.is_adjacent(&shape));
 
-    coords.push((x, y));
+            match last_shape {
+              None => shape,
+              Some(s) => {
+                let mut new_shape = shape;
+
+                new_shape.tl.0 = s.tl.0;
+                new_shape.bl.0 = s.bl.0;
+                new_shape
+              }
+            }
+          }
+        };
+
+        vec.push(new_shape)
+      })
+      .or_insert(vec![shape]);
   }
 
   let mut document = Document::new()
     .set("viewBox", (0, 0, image_data.width(), image_data.height()));
 
-  for (hexcode, coords) in pixel_map.iter() {
+  for (hexcode, shapes) in shape_data {
     match method {
-      Method::Polygons => document.append(to_path_element(hexcode, coords)),
-      Method::Rectangles => document.append(to_rect_element(hexcode, coords)),
-    }
+      Method::Polygons => {
+        let mut path = element::Path::new();
+        let mut data = element::path::Data::new();
+
+        for shape in shapes {
+          data = shape.to_polygon(data);
+        }
+
+        data = data.close();
+        path = path.set("fill", hexcode).set("d", data);
+
+        document.append(path)
+      }
+      Method::Pixels | Method::Blocks => {
+        for shape in shapes {
+          let mut rect = element::Rectangle::new();
+
+          rect = rect.set("fill", hexcode.to_owned());
+          rect = shape.to_rectangle(rect);
+
+          document.append(rect);
+        }
+      }
+    };
   }
 
   svg::write(Output::new(output)?, &document)?;
